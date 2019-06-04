448f9e60a899bb57f181c5b17bd101d806bc6243
Kevin Feyjoo <kf9979a@student.american.edu>
2019-06-04 15:28:58 -0400

    u

---
+++ resize_img.py
@@ -0,0 +1,21 @@
+from PIL import Image
+ 
+def resize_image(input_image_path,
+                 output_image_path,
+                 size):
+    original_image = Image.open(input_image_path)
+    width, height = original_image.size
+    print('The original image size is {wide} wide x {height} '
+          'high'.format(wide=width, height=height))
+ 
+    resized_image = original_image.resize(size)
+    width, height = resized_image.size
+    print('The resized image size is {wide} wide x {height} '
+          'high'.format(wide=width, height=height))
+    resized_image.show()
+    resized_image.save(output_image_path)
+ 
+if __name__ == '__main__':
+    resize_image(input_image_path='resizeMe.jpeg',
+                 output_image_path='resized.jpeg',
+                 size=(800, 400))
